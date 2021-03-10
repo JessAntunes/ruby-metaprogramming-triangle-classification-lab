d34885a6d391eb234cc3cc98bfe3132d7f7317ec
Jessica Antunes <jessantunes@protonmail.com>
2021-03-10 16:07:07 -0500

    commit tree 80394e0983effc893556c3bc613705b5197c035c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,56 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :equilateral, :isosceles, :scalene, :type, :side_c, :side_b, :side_a
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+    @type = kind
+
+  end
+    
+  
+  def kind
+
+    if side_a <=0 || side_b <= 0 || side_c <= 0
+      begin
+        raise TriangleError
+      end
+    elsif side_a + side_b <= side_c || 
+          side_a + side_c <= side_b || 
+          side_c + side_b <= side_a
+      begin
+        raise TriangleError
+      end
+    elsif side_a == side_b && side_b == side_c
+        :equilateral
+    elsif side_a != side_b && side_b != side_c && side_c != side_a
+        :scalene
+    else
+        :isosceles
+    end
+    
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
+
+
 end
+  
+
+
+
+
+
+
+
+
+
+
+
